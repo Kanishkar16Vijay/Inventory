9c0c7c1f525adf4559a177bfdbc86cec70fa24b5
Kanishkar <kanishvijay2005@gmail.com>
2025-04-30 13:54:29 +0530

    Added product_movement file

--- app.py
+++ app.py
@@ -64,5 +64,29 @@ def view_location() :
     connection.close()
     return render_template('view_locations.html',locations=location)
 
+@app.route('/addmovement',methods=['POST','GET'])
+def add_movement() :
+    connection=get_db_connection()
+    cur=connection.cursor(dictionary=True)
+    cur.execute("SELECT *FROM Product")
+    product=cur.fetchall()
+    cur.execute("SELECT *FROM Location")
+    location=cur.fetchall()
+    cur.close()
+    connection.close()
+    if request.method=='POST' :
+        m_id=request.form.get('mid')
+        p_id=request.form.get('pid')
+        from_l=request.form.get('froml')
+        to_l=request.form.get('tol')
+        connection=get_db_connection()
+        cur=connection.cursor()
+        cur.execute("INSERT INTO Movements(movement_id,product_id,from_location,to_location) VALUES(%s,%s,%s,%s)",(m_id,p_id,from_l,to_l))
+        connection.commit()
+        cur.close()
+        connection.close()
+        return render_template('product_movement.html',locations=location,products=product)
+    return render_template('product_movement.html',locations=location,products=product)
+
 if __name__=='__main__' :
     app.run(debug=True)
