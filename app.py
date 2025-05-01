4274cb4c397f32c0a4d3350561422d14f07ff90e
Kanishkar <kanishvijay2005@gmail.com>
2025-05-01 15:42:26 +0530

    Added product movement and edit product file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 import mysql.connector
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app=Flask(__name__)
 
@@ -30,7 +32,7 @@ def add_products() :
         return render_template('add_product.html')
     return render_template('add_product.html')
 
-@app.route('/viewproduct',methods=['GET','POST'])
+@app.route('/viewproduct')
 def view_product() :
     connection=get_db_connection()
     cur=connection.cursor(dictionary=True)
@@ -39,6 +41,26 @@ def view_product() :
     cur.close()
     connection.close()
     return render_template('view_products.html',products=product)
+
+@app.route('/editproduct/<string:id>',methods=['GET','POST'])
+def edit_product(id) :
+    if request.method=='POST' :
+        p_id=request.form.get('pid')
+        p_name=request.form.get('pname')
+        connection=get_db_connection()
+        cur=connection.cursor()
+        cur.execute("SET SQL_SAFE_UPDATES = 0")
+        cur.execute("UPDATE Product SET name=%s WHERE product_id=%s",(p_name,p_id))
+        cur.execute("SET SQL_SAFE_UPDATES = 1")
+        connection.commit()
+        cur.close()
+        connection.close()
+        return redirect(url_for('view_product'))
+    connection=get_db_connection()
+    cur=connection.cursor(dictionary=True)
+    cur.execute("SELECT *FROM Product WHERE product_id=%s",[id])
+    res=cur.fetchone()
+    return render_template('edit_product.html',result=res)
 
 @app.route('/addlocation',methods=['POST','GET'])
 def add_location() :
@@ -54,7 +76,7 @@ def add_location() :
         return render_template('add_location.html')
     return render_template('add_location.html')
 
-@app.route('/viewlocation',methods=['POST','GET'])
+@app.route('/viewlocation')
 def view_location() :
     connection=get_db_connection()
     cur=connection.cursor(dictionary=True)
@@ -78,10 +100,13 @@ def add_movement() :
         m_id=request.form.get('mid')
         p_id=request.form.get('pid')
         from_l=request.form.get('froml')
+        if from_l=="" : from_l=None
         to_l=request.form.get('tol')
+        now_ist = datetime.now(ZoneInfo("Asia/Kolkata"))
+        dt=now_ist.strftime("%Y-%m-%d %H:%M:%S")
         connection=get_db_connection()
         cur=connection.cursor()
-        cur.execute("INSERT INTO Movements(movement_id,product_id,from_location,to_location) VALUES(%s,%s,%s,%s)",(m_id,p_id,from_l,to_l))
+        cur.execute("INSERT INTO Movement(movement_id,product_id,from_location,to_location,Date_Time) VALUES(%s,%s,%s,%s,%s)",(m_id,p_id,from_l,to_l,dt))
         connection.commit()
         cur.close()
         connection.close()
